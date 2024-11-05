e6a3b051fdcea98964cba9144e26c89de691a0a2
Benjamin Martin <000953368@mohawkcollege.ca>
2024-11-05 17:28:27 -0500

    Add Paddle with collisions and Boarders

--- martin-benjamin-a3-game/Game.cs
+++ martin-benjamin-a3-game/Game.cs
@@ -11,14 +11,25 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        Paddle paddle = new Paddle(); 
+        Boarder boarder = new Boarder();    
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Breakout Clone");
+            Window.SetSize(800, 600);
 
+            // Paddle Settings 
+            paddle.position.X = Window.Width / 2;
+            paddle.position.Y = Window.Height - 50;
+            paddle.size = new Vector2(80, 20);
+            paddle.speed = 400; 
+            paddle.color = Color.Black; 
+
+            
         }
 
         /// <summary>
@@ -26,7 +37,11 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
-
+            Window.ClearBackground(Color.White);
+            paddle.DrawPaddle();
+            paddle.MovePaddle();
+            boarder.DrawOuterBoarder(); 
+            boarder.DrawInnerBoarder(); 
         }
     }
 }
---
+++ martin-benjamin-a3-game/Paddle.cs
@@ -0,0 +1,48 @@
+﻿using Raylib_cs;
+using System;
+using System.Numerics;
+
+namespace Game10003
+{
+    public class Paddle
+    {
+        public Vector2 position;
+        public Vector2 size;
+        public Color color;
+        public float speed; 
+
+        public void DrawPaddle()
+        {
+            Draw.LineSize = 1;
+            Draw.FillColor = color;
+            Draw.Rectangle(position, size);
+        }
+
+        public void MovePaddle()
+        {
+            // Move Paddle Left
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left))
+            {
+                position.X -= speed * Time.DeltaTime; 
+            }
+
+            // Move Paddle Right 
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Right))
+            {
+                position.X += speed * Time.DeltaTime;
+            }
+
+            // Constrain Left
+            if (position.X < 100)
+            {
+                position.X = 100;
+            }
+
+            // Constrain Right 
+            if (position.X + size.X > 700)
+            {
+                position.X = 700 - size.X; 
+            }
+        }
+    }
+}
